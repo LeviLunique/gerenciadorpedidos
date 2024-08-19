629caf546bf115ef67b7bb7e4866007d0412eaa1
levi.silva <levi.silva@agu.gov.br>
2024-08-18 21:55:00 -0300

    Create repositories for Produto and Pedido

---
+++ src/main/java/com/xyz/gerenciadorpedidos/repository/PedidoRepository.java
@@ -0,0 +1,12 @@
+package com.xyz.gerenciadorpedidos.repository;
+
+import com.xyz.gerenciadorpedidos.entity.Pedido;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface PedidoRepository extends JpaRepository<Pedido, Long> {
+    List<Pedido> findByStatus(String status);
+}
