508827547d1bfb23cec5487be44dbb26f7d0f0b7
levi.silva <levi.silva@agu.gov.br>
2024-08-18 22:21:59 -0300

    [FEAT] Implement ProdutoServce.java to manage CRUD operations for the Produto.java entity

---
+++ src/main/java/com/xyz/gerenciadorpedidos/service/ProdutoService.java
@@ -0,0 +1,46 @@
+package com.xyz.gerenciadorpedidos.service;
+
+import com.xyz.gerenciadorpedidos.entity.Produto;
+import com.xyz.gerenciadorpedidos.repository.ProdutoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ProdutoService {
+
+    private final ProdutoRepository produtoRepository;
+
+    @Autowired
+    public ProdutoService(ProdutoRepository produtoRepository) {
+        this.produtoRepository = produtoRepository;
+    }
+
+    public List<Produto> findAll() {
+        return produtoRepository.findAll();
+    }
+
+    public Optional<Produto> findById(Long id) {
+        return produtoRepository.findById(id);
+    }
+
+    public Produto save(Produto produto) {
+        return produtoRepository.save(produto);
+    }
+
+    public Produto update(Long id, Produto produtoAtualizado) {
+        return produtoRepository.findById(id).map(produto -> {
+            produto.setNome(produtoAtualizado.getNome());
+            produto.setDescricao(produtoAtualizado.getDescricao());
+            produto.setPreco(produtoAtualizado.getPreco());
+            return produtoRepository.save(produto);
+        }).orElseThrow(() -> new RuntimeException("Produto não encontrado"));
+    }
+
+    public void deleteById(Long id) {
+        produtoRepository.deleteById(id);
+    }
+
+}
